c9da930ebdfc754f2b653604fe116e89d5fec689
Asbjørn Henriksen <1065511@ucn.dk>
2019-05-30 19:20:39 +0200

    blabla

--- CiiMacClientMVC/Controllers/UserController.cs
+++ CiiMacClientMVC/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Web;
 using System.Web.Mvc;
 using Proxy;
@@ -11,20 +12,26 @@ namespace CiiMacClientMVC.Controllers
     public class UserController : Controller
     {
         // GET: User
-        
+
         public ActionResult UserHome()
         {
             return View();
         }
 
-        [HttpPost]
         public ActionResult InsertUser(User user, string password)
         {
-            UserClient userClient = new UserClient();
+            try
+            {
+                UserClient userClient = new UserClient();
 
-            userClient.CreateUser(user, password);
+                userClient.CreateUser(user, password);
+            }
+            catch (FaultException ex)
+            {
+                ex.StackTrace.ToString();
+            }
 
-            return View(); 
+            return View();
         }
 
     }
